8cb9878c69819e99335ceafb08ba15d92bd6c646
Felix Kipkurui Langat <fexlangat@gmail.com>
2024-08-12 14:20:27 +0300

    Added counties

--- config/middlewares.js
+++ config/middlewares.js
@@ -10,3 +10,39 @@ module.exports = [
   'strapi::favicon',
   'strapi::public',
 ];
+
+
+
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   {
+//     name: 'strapi::security',
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           'connect-src': ["'self'", 'https:'],
+//           'img-src': ["'self'", 'data:', 'blob:', 'https://ba2b-154-152-238-181.ngrok-free.app'],
+//           'media-src': ["'self'", 'data:', 'blob:', 'https://ba2b-154-152-238-181.ngrok-free.app'],
+//           upgradeInsecureRequests: null,
+//         },
+//       },
+//     },
+//   },
+//   {
+//     name: 'strapi::cors',
+//     config: {
+//       enabled: true,
+//       origin: ['http://localhost:3000', 'https://ba2b-154-152-238-181.ngrok-free.app'],
+//       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+//       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+//     },
+//   },
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
