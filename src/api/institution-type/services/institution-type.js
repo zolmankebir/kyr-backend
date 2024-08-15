78b02fbee566d5bc75256f03cb6c2d3852e576d6
Georgemugo <georgepmash@gmail.com>
2024-08-15 16:26:55 +0300

    updates

---
+++ src/api/institution-type/services/institution-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * institution-type service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::institution-type.institution-type');
---
+++ src/api/ministry/controllers/ministry.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ministry controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ministry.ministry');
