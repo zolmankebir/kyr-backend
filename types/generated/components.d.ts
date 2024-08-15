205448d0bc19cb45a2c0c43bcb2e07d6fe131385
Georgemugo <georgepmash@gmail.com>
2024-08-15 16:32:50 +0300

    updates

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,19 +1,5 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface RepresentativeInfoRepresentativeInfo extends Schema.Component {
-  collectionName: 'components_representative_info_representative_infos';
-  info: {
-    displayName: 'Representative_Info';
-    icon: 'book';
-  };
-  attributes: {
-    Name: Attribute.String;
-    Role: Attribute.Enumeration<['Senator', 'Mp', 'MCA']>;
-    Party: Attribute.String;
-    Photo: Attribute.Media<'images' | 'videos'>;
-  };
-}
-
 export interface ContactContactInfo extends Schema.Component {
   collectionName: 'components_contact_contact_infos';
   info: {
@@ -28,11 +14,25 @@ export interface ContactContactInfo extends Schema.Component {
   };
 }
 
+export interface RepresentativeInfoRepresentativeInfo extends Schema.Component {
+  collectionName: 'components_representative_info_representative_infos';
+  info: {
+    displayName: 'Representative_Info';
+    icon: 'book';
+  };
+  attributes: {
+    Name: Attribute.String;
+    Role: Attribute.Enumeration<['Senator', 'Mp', 'MCA']>;
+    Party: Attribute.String;
+    Photo: Attribute.Media<'images' | 'videos'>;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'representative-info.representative-info': RepresentativeInfoRepresentativeInfo;
       'contact.contact-info': ContactContactInfo;
+      'representative-info.representative-info': RepresentativeInfoRepresentativeInfo;
     }
   }
 }
